8f710ef18b1516d68f5bae1d908c156f104aed8a
ishakikani9117 <ishakikani999@gmail.com>
2024-08-12 00:38:42 -0700

    corrected path for Form component

--- app/edit-prompt/page.jsx
+++ app/edit-prompt/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import Form from '@components/Form';
+import Form from '@components/form';
 
 const EditPrompt = () => {
   const router = useRouter();
